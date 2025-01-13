2c60ae3704abdad8ea4b746c1edd83684e40f2a8
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:21:46 +0100

    python

--- factorial.py
+++ factorial.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2:
         print("Usage: ./factorial.py <integer>")
         sys.exit(1)
 
-    try:
-        num = int(sys.argv[1])
-        if num < 0:
-            print("Error: Factorial is not defined for negative numbers.")
-        else:
-            print(factorial(num))
-    except ValueError:
-        print("Error: Please provide a valid integer as input.")
+    print(" ".join(sys.argv[1:]))
