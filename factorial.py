add5b6720c5e8cee5a9391aa522cdd35a5d01bfb
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:27:41 +0100

    python

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  
     return result
 
 if __name__ == "__main__":
