0353a4f207b43f46888353e645791704b64235df
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:31:47 +0100

    python

--- factorial.py
+++ factorial.py
@@ -5,9 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  
+        n -= 1  # Decrement n to avoid infinite loop
     return result
 
-if __name__ == "__main__":
-    f = factorial(int(sys.argv[1]))
-    print(f)
+f = factorial(int(sys.argv[1]))
+print(f)
