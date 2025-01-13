208f548063a6eb829385b31229d900cef5a6f8c6
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:37:58 +0100

    chat

--- factorial.py
+++ factorial.py
@@ -5,15 +5,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  
+        n -= 1  # Decrement n to ensure the loop terminates
     return result
 
-if len(sys.argv) < 2:
-    print("Usage: ./factorial.py <integer>")
-    sys.exit(1)
-
-try:
-    num = int(sys.argv[1])
-    print(factorial(num))
-except ValueError:
-    print("Error: Please provide a valid integer as input.")
+f = factorial(int(sys.argv[1]))
+print(f)
