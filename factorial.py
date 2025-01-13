2945a97accfe6480de4f68db71c6f2e6c1871763
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:07:50 +0100

    python

--- factorial.py
+++ factorial.py
@@ -5,21 +5,16 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  
+        n = n - 1
     return result
 
 if len(sys.argv) != 2:
-    print("Usage: python3 script.py <non-negative-integer>")
+    print("Usage: ./factorial.py <number>")
     sys.exit(1)
 
 try:
-    num = int(sys.argv[1])
-    if num < 0:
-        print("Error: Please provide a non-negative integer.")
-        sys.exit(1)
+    f = factorial(int(sys.argv[1]))
+    print(f)
 except ValueError:
-    print("Error: Please provide a valid integer.")
+    print("Error: Invalid input. Please provide a valid integer.")
     sys.exit(1)
-
-f = factorial(num)
-print(f)
