0c9f69f51301a8cee8b4005f21c4e6ccc3826940
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:10:20 +0100

    python

--- factorial.py
+++ factorial.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
+
 import sys
 
 def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n = n - 1
+        n -= 1  
     return result
+f = factorial(int(sys.argv[1]))
+print(f)
 
-if len(sys.argv) != 2:
-    print("Usage: ./factorial.py <number>")
-    sys.exit(1)
-
-try:
-    f = factorial(int(sys.argv[1]))
-    print(f)
-except ValueError:
-    print("Error: Invalid input. Please provide a valid integer.")
-    sys.exit(1)
