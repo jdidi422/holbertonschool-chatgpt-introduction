1d9f33e610d5c26855a7472d0b89398dbcb45ba2
jdidi422 <rassiljdidi3@gmail.com>
2025-01-13 16:51:44 +0100

    chat

--- factorial.py
+++ factorial.py
@@ -6,7 +6,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrement n to avoid infinite loop
+        n -= 1  
     return result
 
 f = factorial(int(sys.argv[1]))
